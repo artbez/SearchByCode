186ef329af8f65b6a25425383b84b12bec4231c1
artemijbezguzikov <artemii.bezguzikov@gmail.com>
2018-06-13 02:38:17 +0300

    Initial

---
+++ frontend/src/main/kotlin/iimetra.infosearch.searchbycode/Index.kt
@@ -0,0 +1,12 @@
+package iimetra.infosearch.searchbycode
+
+import kotlinext.js.invoke
+import react.dom.render
+import kotlin.browser.document
+
+fun main(args: Array<String>) {
+    kotlinext.js.require.invoke("styles/common.scss")
+    render(document.getElementById("root")) {
+        +"Hello, world"
+    }
+}
